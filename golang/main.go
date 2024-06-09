e422bf8d66634914e1209c5d1cb3f4ef55740d93
t_takumaru <t.takuma.saga.u@gmail.com>
2024-06-10 00:57:26 +0900

    gormでmysqlの接続に成功

--- golang/main.go
+++ golang/main.go
@@ -2,13 +2,30 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"net/http"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
+	// DSN (Data Source Name) の詳細は https://github.com/go-sql-driver/mysql#dsn-data-source-name を参照
+	// [ユーザ名]:[パスワード]@tcp([ホスト名]:[ポート番号])/[データベース名]?charset=[文字コード]
+	dsn := fmt.Sprintf(`%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local`,
+		os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_NAME"))
+
+	fmt.Println("dsn:", dsn)
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		fmt.Println("DB接続に失敗しました:", err)
+		return
+	}
+	fmt.Println("DB接続に成功しました:", db)
+
 	fmt.Println("Hello, World!")
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
