0f576075ac7ce0cc492907f7e9358f641a3dfc54
t_takumaru <t.takuma.saga.u@gmail.com>
2024-06-09 23:09:18 +0900

    echoが動くように

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
 
 func main() {
 	fmt.Println("Hello, World!")
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	e.Logger.Fatal(e.Start(":8080"))
+
 }
