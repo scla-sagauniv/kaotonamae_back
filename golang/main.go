fa0c6195354ace29a03fbfd1c4936f203cacdc8e
t_takumaru <t.takuma.saga.u@gmail.com>
2024-06-10 01:11:34 +0900

    テーブルの追加、テスト成功

--- golang/main.go
+++ golang/main.go
@@ -12,7 +12,17 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-func main() {
+// MySQLに作成するUserテーブルの定義
+type User struct {
+	// gorm.Modelをつけると、idとCreatedAtとUpdatedAtとDeletedAtが作られる
+	gorm.Model
+
+	Name string
+	Age  int
+}
+
+// DBを起動させる
+func dbInit() *gorm.DB {
 	// DSN (Data Source Name) の詳細は https://github.com/go-sql-driver/mysql#dsn-data-source-name を参照
 	// [ユーザ名]:[パスワード]@tcp([ホスト名]:[ポート番号])/[データベース名]?charset=[文字コード]
 	dsn := fmt.Sprintf(`%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local`,
@@ -22,14 +32,40 @@ func main() {
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		fmt.Println("DB接続に失敗しました:", err)
-		return
 	}
 	fmt.Println("DB接続に成功しました:", db)
 
-	fmt.Println("Hello, World!")
+	// 接続に成功した場合、「db connected!!」と表示する
+	fmt.Println("db connected!!")
+	return db
+}
+
+func main() {
+	// // DSN (Data Source Name) の詳細は https://github.com/go-sql-driver/mysql#dsn-data-source-name を参照
+	// // [ユーザ名]:[パスワード]@tcp([ホスト名]:[ポート番号])/[データベース名]?charset=[文字コード]
+	// dsn := fmt.Sprintf(`%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local`,
+	// 	os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_NAME"))
+
+	// fmt.Println("dsn:", dsn)
+	// db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	// if err != nil {
+	// 	fmt.Println("DB接続に失敗しました:", err)
+	// 	return
+	// }
+	// fmt.Println("DB接続に成功しました:", db)
+
+	// DB起動
+	db := dbInit()
+
+	// fmt.Println("Hello, World!")
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
+	})
+	e.GET("/db_test", func(c echo.Context) error {
+		// Userテーブル作成
+		db.AutoMigrate(&User{})
+		return c.String(http.StatusOK, "test")
 	})
 	e.Logger.Fatal(e.Start(":8080"))
 
