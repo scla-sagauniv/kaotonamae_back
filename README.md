64bd242b52df0616f583010f740350ffb5ac20c2
t_takumaru <t.takuma.saga.u@gmail.com>
2024-06-10 17:17:21 +0900

    環境変数のportの指定が良くなかった

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # 顔と名前が一致しない＿バックエンド
+
+### 環境変数
+rootディレクトリに.env
+
+```
+DB_HOST=db
+DB_PORT=3306
+DB_USER=testuser
+DB_PASSWORD=testpassword
+DB_NAME=test_database
+DB_ROOT_PASSWORD=rootpassword
+```
--- golang/main.go
+++ golang/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 
 	"net/http"
@@ -12,50 +13,44 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-// MySQLに作成するUserテーブルの定義
 type User struct {
-	// gorm.Modelをつけると、idとCreatedAtとUpdatedAtとDeletedAtが作られる
-	gorm.Model
-
-	Name string
-	Age  int
+	ID    uint   `gorm:"primaryKey"`
+	Name  string `gorm:"size:100"`
+	Email string `gorm:"size:100;unique"`
 }
 
 // DBを起動させる
-func dbInit() *gorm.DB {
+func gormConnect() (*gorm.DB, error) {
 	// DSN (Data Source Name) の詳細は https://github.com/go-sql-driver/mysql#dsn-data-source-name を参照
 	// [ユーザ名]:[パスワード]@tcp([ホスト名]:[ポート番号])/[データベース名]?charset=[文字コード]
-	dsn := fmt.Sprintf(`%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local`,
+	dsn := fmt.Sprintf(`%s:%s@tcp(%s:%s)/%s`,
 		os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_NAME"))
 
 	fmt.Println("dsn:", dsn)
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		fmt.Println("DB接続に失敗しました:", err)
+	} else {
+		fmt.Println("DB接続に成功しました:", db)
 	}
-	fmt.Println("DB接続に成功しました:", db)
 
-	// 接続に成功した場合、「db connected!!」と表示する
-	fmt.Println("db connected!!")
-	return db
+	return db, nil
 }
 
 func main() {
-	// // DSN (Data Source Name) の詳細は https://github.com/go-sql-driver/mysql#dsn-data-source-name を参照
-	// // [ユーザ名]:[パスワード]@tcp([ホスト名]:[ポート番号])/[データベース名]?charset=[文字コード]
-	// dsn := fmt.Sprintf(`%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local`,
-	// 	os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_NAME"))
+	// // DB起動
+	// db := dbInit()
 
-	// fmt.Println("dsn:", dsn)
-	// db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	// if err != nil {
-	// 	fmt.Println("DB接続に失敗しました:", err)
-	// 	return
-	// }
-	// fmt.Println("DB接続に成功しました:", db)
+	db, err := gormConnect()
+	if err != nil {
+		log.Fatalf("could not connect to the database: %v", err)
+	}
 
-	// DB起動
-	db := dbInit()
+	// マイグレーション
+	err = db.AutoMigrate(&User{})
+	if err != nil {
+		log.Fatalf("failed to migrate database: %v", err)
+	}
 
 	// fmt.Println("Hello, World!")
 	e := echo.New()
